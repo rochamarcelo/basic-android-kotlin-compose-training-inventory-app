569d88e50310c7a90c02cbf164c2f800fe567720
Marcelo Rocha <contato@omarcelo.com.br>
2025-05-13 19:35:03 -0300

    Create an item Entity

--- app/src/main/java/com/example/inventory/data/Item.kt
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -16,12 +16,13 @@
 
 package com.example.inventory.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-/**
- * Entity data class represents a single row in the database.
- */
-class Item(
-    val id: Int = 0,
+
+@Entity(tableName = "items")
+data class Item(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val name: String,
     val price: Double,
     val quantity: Int
